e931f0dcb1f4b15a1e9494a6f019106392691835
Michael Cacciatore <mcacciatore116@gmail.com>
2019-10-05 12:08:32 -0400

    fix getDisplayName

--- src/utils/getDisplayName.js
+++ src/utils/getDisplayName.js
@@ -1,7 +1,13 @@
 const regExp = /\(([^)]+)\)/;
 
+/**
+ * Strips out the actual display name from a component assuming it is wrapped in parentheses as is common
+ * in libraries such as redux where the displayName is `ConnectedComponent(ActualComponentName)`
+ * @param {string} name The displayName of the component
+ * @returns {string} The actual displayName of the component
+ */
 export default name => {
-  const result = regExp.exec(name);
+  const result = regExp.exec(name) || [null, name];
 
   return result[1] || 'Anonymous';
 };
